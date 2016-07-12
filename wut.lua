925cfcc1b02f5cdd29946dcf5ad493d5944a8ef6
taptap <alexey.e.egorov@gmail.com>
2016-07-12 11:58:56 +0300

    make a nice animation while music is playing
    Just some ANSI terminal color codes, nothing special.
    And \r with io/write, which is a nice way to make constantly overwting
    lines.
    
    Here's a link: https://en.wikipedia.org/wiki/ANSI_escape_code#Colors

--- wut.lua
+++ wut.lua
@@ -8,5 +8,19 @@ print("Press ^C when tired, or when music ends.")
 print("Since we made xmplay.play asynchronous, this script just ends,\
 if we don't sleep or spin in endless loop")
 
+local position = 1
+local len = 77
+local cyan = string.char(27) .. "[36m"
+local reset = string.char(27) .. "[0m"
+local step = 1
+
 while true do
+   local prefix = string.rep(" ", position)
+   local suffix = string.rep(" ", len - position)
+   io.write("[" ..  prefix .. cyan .. "***" .. reset .. suffix .. "]\r")
+   io.flush()
+   position = position + step
+   if position == len - 1 or position == 0 then
+      step = -step
+   end
 end
